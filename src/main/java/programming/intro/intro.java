6996c0de8c0925098f5a24f80d99e1b7b0058549
Martellan <martiyordanov92@gmail.com>
2023-01-23 12:57:00 +0200

    Added main method and some...

--- src/main/java/programming/intro/intro.java
+++ src/main/java/programming/intro/intro.java
@@ -1,4 +1,20 @@
 package programming.intro;
 
+import java.util.Scanner;
+
 public class intro {
+    public static void main(String [] args){
+        System.out.println("Please enter your name");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("hi, " +  name);
+        System.out.println("Please enter your age");
+        int age = scanner.nextInt();
+        if (age>=18){
+            System.out.println(name + " is able to drink");
+        }else {
+            System.out.println(name + " is NOT able to drink");
+        }
+
+    }
 }
